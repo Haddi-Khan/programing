bc22be89f80a80fb3b6c859d8128be392a1d33df
haddi khan <haddik011@gmail.com>
2025-08-12 03:16:08 +0500

    Add files via upload

---
+++ calulator.py
@@ -0,0 +1,7 @@
+a=6
+b=5
+print ("The ans of",a,"+",b,"=",a+b)
+print ("The ans of",a,"-",b,"=",a-b)
+print ("The ans of",a,"*",b,"=",a*b)
+print ("The ans of",a,"/",b,"=",a/b)
+print ("The ans of",a,"%",b,"=",a%b)
