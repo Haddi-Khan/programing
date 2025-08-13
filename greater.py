984cad8342cee2a7eb51d3f9462219fcc5bbe531
haddi khan <haddik011@gmail.com>
2025-08-14 00:31:52 +0500

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,8 @@
+a = float(input("Enter any number: "))
+b = float(input("enter any number: "))
+if a  > b:
+    print (a,"is a larger number than the b",b)
+elif b > a:
+    print (b,"is a larger number than the a", a)
+elif a ==b:
+    print ("both are equal numbers")
