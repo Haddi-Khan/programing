92f7a2f83c738ac4a28d5c1ab224198dd5ef9433
haddi khan <haddik011@gmail.com>
2025-08-16 21:28:07 +0500

    Delete positive,negative.py

--- positive,negative.py
+++
@@ -1,8 +0,0 @@
-# 5.	Positive, Negative, or Zero – Determine if a number is positive, negative, or zero.
-a = float(input("Enter any number: "))
-if a > 0:
-    print (a,"is a positive number")
-elif a < 0:
-    print (a,"is a negative number")
-else:
-    print("number is zero")
